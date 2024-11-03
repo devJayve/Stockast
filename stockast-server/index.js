b72192dbf25dfe410dce65eb3361bdd7e37a57f7
DevJayve <wndud4576@naver.com>
2024-11-03 18:00:36 +0900

    build: node express 서버 구축

---
+++ stockast-server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+    res.send('/ : test');
+})
+
+app.listen(PORT, () => {
+    console.log(`${PORT} : listen`);
+});
+
+
+
