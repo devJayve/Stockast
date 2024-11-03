0980bee090eb31aa6dcf4ac2fc2c72535989f54c
DevJayve <wndud4576@naver.com>
2024-11-03 17:49:25 +0900

    chore: 폴더 구조 재배치

--- stockast-web/src/pages/Home/StockChart.tsx
+++ stockast-web/src/pages/Home/StockChart.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import StockIndex from './StockIndex';
 
 const StockChart = () => {
-  return <div>실시간 차트</div>;
+  return (
+    <div>
+      <p>실시간 차트</p>
+    </div>
+  );
 };
 
 export default StockChart;
